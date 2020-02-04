7bb11eae9eb4f6f130bcab31d7410999120e4d80
sofiyamamchych <sofiyamamchich@gmail.com>
2020-02-04 20:47:15 +0200

    navbar style draft
    - img added
    - font added
    - gulp will copy img and fonts to dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,24 @@ const { parallel } = require('gulp');
 
 
 function convertSass() {
-  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('src/scss/**/*.scss') // Gets all files ending with .scss in src/scss and children dirs
     .pipe(sass())
     .pipe(gulp.dest('dist'))
 }
+
 function copyHtml() {
-  return gulp.src('src/index.html') // Gets all files ending with .scss in app/scss and children dirs
+  return gulp.src('src/index.html') // Copy HTML to dist
     .pipe(gulp.dest('dist'))
 }
 
-exports.build = parallel(convertSass, copyHtml);
+function copyImg() {
+  return gulp.src('src/img/**/*') // Copy img to dist
+    .pipe(gulp.dest('dist/img'))
+}
+
+function copyFonts() {
+  return gulp.src('src/fonts/**/*') // Copy font to dist
+    .pipe(gulp.dest('dist/fonts'))
+}
+
+exports.build = parallel(convertSass, copyHtml, copyImg, copyFonts);
